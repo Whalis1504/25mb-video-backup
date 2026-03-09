c41b1f91fadb8f62f83f4f62b854cc7ea8316ac8
BlueG15 <015042005minh@gmail.com>
2026-03-09 20:35:40 +0700

    Reduced file sizes

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const thumbSrc = "thumb/"
 const DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21].map(id => {
     return {
         id,
-        "img" : thumbSrc + id + ".png",
+        "img" : thumbSrc + id + ".webp",
         "src" : src + id + ".webm",
     }
 })
