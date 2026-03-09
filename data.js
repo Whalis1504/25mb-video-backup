fc0d09b1478e524c9721ee408aa4d3824675e4c8
BlueG15 <015042005minh@gmail.com>
2026-03-09 13:06:00 +0700

    Add 19, 20, 21, done batch 1

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const src = "vids/"
 const thumbSrc = "thumb/"
-const DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(id => {
+const DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21].map(id => {
     return {
         id,
         "img" : thumbSrc + id + ".png",
