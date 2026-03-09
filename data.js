739990cc61fedee5ea16fd56f6d0a8832441c8c8
BlueG15 <015042005minh@gmail.com>
2026-03-09 12:52:50 +0700

    Add 6, 7, 8

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const src = "vids/"
 const thumbSrc = "thumb/"
-const DATA = [1, 2, 3, 4, 5, 12].map(id => {
+const DATA = [1, 2, 3, 4, 5, 6, 7, 8, 12].map(id => {
     return {
         id,
         "img" : thumbSrc + id + ".png",
